20fff48fcd52b6d3ad5f759f113f9a15038ddbda
aghinea <aghinea@bitdefender.com>
2023-11-15 13:07:19 +0200

    Couple of additions

--- src/com/cebp_project/messaging/server/Server.java
+++ src/com/cebp_project/messaging/server/Server.java
@@ -12,7 +12,35 @@ public class Server implements Runnable {
     @Override
     public void run() {
         while (true) {
-            // The server can perform additional tasks here
+            try {
+                // Process messages from the main message queue
+                processMessages();  // TODO: Also add for the Topics?
+
+                // Additional tasks like client health checks, logging, etc.
+                performHealthChecks();
+                performLogging();
+
+                // Simulate a delay
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
         }
     }
+
+    private void processMessages() {
+        // Implement message routing logic
+        // Here's where the Kafka/RabbitMQ routing would go
+    }
+
+    private void performHealthChecks() {
+        // TODO: Implement server health checks
+        //  Could include checking client connectivity, server resource usage, etc.
+    }
+
+    private void performLogging() {
+        // TODO
+    }
 }
+
