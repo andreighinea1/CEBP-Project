bf054f18d01d5fa14b72cf8e5dc7a587dfee0ed8
bianca <bianca.diaconu0120@gmail.com>
2023-12-04 23:52:19 +0200

    Move Rabbitmq to a separate class-still not working

--- src/main/java/com/cebp_project/viral_service/ViralService.java
+++ src/main/java/com/cebp_project/viral_service/ViralService.java
@@ -1,142 +1,77 @@
 package com.cebp_project.viral_service;
 
-// TODO-ale-last?: After the RabbitMQ is implemented, all these imports should be removed, and the DTO should be used
+import com.cebp_project.dto.MessageQueueDTO;
 import com.cebp_project.messenger.message.Message;
-import com.cebp_project.messenger.message.MessageQueue;
 import com.cebp_project.messenger.topic.TopicMessage;
-import com.cebp_project.messenger.topic.TopicOrchestrator;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+import com.cebp_project.rabbitmq.RabbitMQManager;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeoutException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ViralService implements Runnable {
-    // TODO-bia-1: Don't use semaphores anymore, just duplicate the msg and send it to RabbitMQ
-    //  NOTE: Only use RabbitMQ for the connection between the Server and ViralService
-
-    // TODO-ale-1: Make a DTO for transferring messages in the MessageQueue,
-    //  and another one for the ones in TopicOrchestrator (this will be used to communicate with RabbitMQ)
-
-    // TODO-deea-1: Make a main method to start this in a separate "admin" process
-
-    private static final ViralService instance = new ViralService();
     private final ConcurrentHashMap<String, Integer> broadcastHashtagCounts = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<String, Integer> topicHashtagCounts = new ConcurrentHashMap<>();
     private final Set<Message> processedMessages = ConcurrentHashMap.newKeySet();
     private final Set<TopicMessage> processedTopicMessages = ConcurrentHashMap.newKeySet();
-    private final Semaphore newMessageSemaphore = new Semaphore(0);  // Used to indicate new messages
 
     private final static String BROADCAST_QUEUE_NAME = "broadcast_queue";
     private final static String TOPIC_QUEUE_NAME = "topic_queue";
-    private Connection connection;
-    private Channel channel;
+    private final RabbitMQManager rabbitMQManager;
 
-    private void setupRabbitMQ() {
-        try {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");  // Replace with the actual host
-            // factory.setUsername("username");  // Uncomment if you have username
-            // factory.setPassword("password");  // Uncomment if you have password
-            connection = factory.newConnection();
-            channel = connection.createChannel();
-
-            // Declare the queues
-            channel.queueDeclare(BROADCAST_QUEUE_NAME, false, false, false, null);
-            channel.queueDeclare(TOPIC_QUEUE_NAME, false, false, false, null);
-        } catch (Exception e) {
-            e.printStackTrace(); // Log the exception
-        }
+    public ViralService() throws IOException, TimeoutException {
+        rabbitMQManager = new RabbitMQManager(BROADCAST_QUEUE_NAME, TOPIC_QUEUE_NAME);
     }
-    public static ViralService getInstance() {
-        return instance;
-    }
-
-
-    // Overloaded method to handle TopicMessage
-    public void notifyNewMessage(Message message) throws IOException {
-        if (channel == null) {
-            setupRabbitMQ();  // Attempt to re-establish connection
-            if (channel == null) {
-                throw new IllegalStateException("RabbitMQ channel is not initialized.");
-            }
-        }
-        String messageJson = convertMessageToJson(message);
-        channel.basicPublish("", BROADCAST_QUEUE_NAME, null, messageJson.getBytes());
-    }
-
-
-    public void notifyNewTopicMessage(TopicMessage message) throws IOException {
-        String messageJson = convertTopicMessageToJson(message); // Serialize the message
-        channel.basicPublish("", TOPIC_QUEUE_NAME, null, messageJson.getBytes());
-    }
-
 
     @Override
     public void run() {
         try {
-            setupRabbitMQ();
-
-            DeliverCallback broadcastCallback = (consumerTag, delivery) -> {
-                String messageJson = new String(delivery.getBody(), "UTF-8");
-                Message message = convertJsonToMessage(messageJson); // Deserialize the message
-                processBroadcastMessage(message);
-            };
-            channel.basicConsume(BROADCAST_QUEUE_NAME, true, broadcastCallback, consumerTag -> {});
-
-            DeliverCallback topicCallback = (consumerTag, delivery) -> {
-                String messageJson = new String(delivery.getBody(), "UTF-8");
-                TopicMessage message = convertJsonToTopicMessage(messageJson); // Deserialize the message
-                processTopicMessage(message);
-            };
-            channel.basicConsume(TOPIC_QUEUE_NAME, true, topicCallback, consumerTag -> {});
-
+            rabbitMQManager.consumeMessages(BROADCAST_QUEUE_NAME, this::processBroadcastMessageJson);
+            rabbitMQManager.consumeMessages(TOPIC_QUEUE_NAME, this::processTopicMessageJson);
         } catch (Exception e) {
             e.printStackTrace();
-            try {
-                if (connection != null) connection.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
         }
     }
-    private String convertMessageToJson(Message message) {
-        // Implement serialization logic, possibly using a library like Gson or Jackson
-        return "";  // Placeholder
+
+    private void processBroadcastMessageJson(String messageJson) {
+        Message message = convertJsonToMessage(messageJson);
+        if (message != null) {
+            processBroadcastMessage(message);
+        }
+    }
+
+    private void processTopicMessageJson(String messageJson) {
+        TopicMessage message = convertJsonToTopicMessage(messageJson);
+        if (message != null) {
+            processTopicMessage(message);
+        }
     }
 
     private Message convertJsonToMessage(String json) {
-        // Implement deserialization logic
-        return new Message("senderId","recipientId","Hello, this is a message!",16);  // Placeholder
-    }
-
-    private String convertTopicMessageToJson(TopicMessage message) {
-        // Implement serialization logic
-        return "";  // Placeholder
+        MessageQueueDTO dto = MessageQueueDTO.fromJson(json);
+        if (dto == null) {
+            return null;
+        }
+        return new Message(dto.getSender(), dto.getRecipient(), dto.getContent(), dto.getTimestamp());
     }
 
     private TopicMessage convertJsonToTopicMessage(String json) {
-        // Implement deserialization logic
-        return new TopicMessage("someTopicType","This is the content of the topic message.");  // Placeholder
+        // Implement deserialization logic for TopicMessage
+        // Placeholder - replace with actual implementation
+        return new TopicMessage("someTopicType", "This is the content of the topic message.");
     }
+
     public void stopService() {
         try {
-            if (channel != null) channel.close();
-            if (connection != null) connection.close();
+            rabbitMQManager.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-
 
     private void processBroadcastMessage(Message message) {
         if (!processedMessages.contains(message)) {
