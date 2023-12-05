6538484f0b26e967f8ea3268bd2b678d6b0b183a
AndreeaFM <deea.fruja@yahoo.com>
2023-12-05 13:05:45 +0200

    rabbit mq

---
+++ src/main/java/com/cebp_project/rabbitmq/RabbitMQManager.java
@@ -0,0 +1,64 @@
+package com.cebp_project.rabbitmq;
+
+import com.cebp_project.dto.MessageQueueDTO;
+import com.cebp_project.messenger.message.Message;
+import com.cebp_project.messenger.topic.TopicMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Consumer;
+
+public class RabbitMQManager {
+    private final String broadcastQueueName;
+    private final String topicQueueName;
+    private Channel channel;
+    private Connection connection;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public RabbitMQManager(String broadcastQueueName, String topicQueueName) throws IOException, TimeoutException {
+        this.broadcastQueueName = broadcastQueueName;
+        this.topicQueueName = topicQueueName;
+        setupRabbitMQ();
+    }
+
+    private void setupRabbitMQ() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost"); // Replace with the actual host
+        // factory.setUsername("username");  // Uncomment if you have username
+        // factory.setPassword("password");  // Uncomment if you have password
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+
+        // Declare the queues
+        channel.queueDeclare(broadcastQueueName, false, false, false, null);
+        channel.queueDeclare(topicQueueName, false, false, false, null);
+    }
+
+    public void publishMessage(Message message) throws IOException {
+        String messageJson = objectMapper.writeValueAsString(message);
+        channel.basicPublish("", broadcastQueueName, null, messageJson.getBytes());
+    }
+
+    public void publishTopicMessage(TopicMessage message) throws IOException {
+        String messageJson = objectMapper.writeValueAsString(message);
+        channel.basicPublish("", topicQueueName, null, messageJson.getBytes());
+    }
+
+    public void consumeMessages(String queueName, Consumer<String> messageProcessor) throws IOException {
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            messageProcessor.accept(message);
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
+    }
+
+    public void close() throws IOException, TimeoutException {
+        if (channel != null) channel.close();
+        if (connection != null) connection.close();
+    }
+
+    // Helper methods for message conversion (if needed)
+    // ...
+}
