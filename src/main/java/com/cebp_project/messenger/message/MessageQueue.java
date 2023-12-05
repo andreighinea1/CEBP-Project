f930892b874dcbf96bc19a83997a229267afd39f
bianca <bianca.diaconu0120@gmail.com>
2023-12-05 17:06:47 +0200

    Fix both message queue and topic orchestrator classes by using the RabbitmqManager

--- src/main/java/com/cebp_project/messenger/message/MessageQueue.java
+++ src/main/java/com/cebp_project/messenger/message/MessageQueue.java
@@ -1,29 +1,44 @@
 package com.cebp_project.messenger.message;
 
-import com.cebp_project.viral_service.ViralService;
-
+import com.cebp_project.rabbitmq.RabbitMQManager;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeoutException;
 
 public class MessageQueue {
-    private static final MessageQueue instance = new MessageQueue(100);
+    private static MessageQueue instance;
     private final BlockingQueue<Message> queue;
+    private final RabbitMQManager rabbitMQManager;
 
+    // Private constructor to handle exceptions from RabbitMQManager.getInstance()
     private MessageQueue(int maxSize) {
         this.queue = new LinkedBlockingQueue<>(maxSize);
+        try {
+            this.rabbitMQManager = RabbitMQManager.getInstance();
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException("Unable to initialize RabbitMQManager", e);
+        }
     }
 
-    public static MessageQueue getInstance() {
+    // Static method for getting the singleton instance
+    public static synchronized MessageQueue getInstance() {
+        if (instance == null) {
+            instance = new MessageQueue(100); // Default size for the queue
+        }
         return instance;
     }
 
-    public void sendMessage(Message message) throws IllegalStateException, IOException {
+    public void sendMessage(Message message) throws IllegalStateException {
         queue.add(message);
-        ViralService.getInstance().notifyNewMessage(message); // Notify the Viral service
+        try {
+            rabbitMQManager.publishMessage(message); // Publish to RabbitMQ
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to publish message", e);
+        }
     }
 
     public Message receiveMessage(String recipient) {
--- src/main/java/com/cebp_project/messenger/topic/TopicOrchestrator.java
+++ src/main/java/com/cebp_project/messenger/topic/TopicOrchestrator.java
@@ -1,59 +1,74 @@
 package com.cebp_project.messenger.topic;
 
-import com.cebp_project.viral_service.ViralService;
+import com.cebp_project.rabbitmq.RabbitMQManager;
+import com.cebp_project.messenger.topic.TopicMessage;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeoutException;
 
 public class TopicOrchestrator {
-    private static final TopicOrchestrator instance = new TopicOrchestrator(5000);
+    private static TopicOrchestrator instance;
     private final ConcurrentHashMap<String, ConcurrentLinkedQueue<TopicMessage>> topicMessages;
     private final long maxTimeout;
+    private final RabbitMQManager rabbitMQManager;
 
     private TopicOrchestrator(long maxTimeout) {
         this.maxTimeout = maxTimeout;
         this.topicMessages = new ConcurrentHashMap<>();
+        try {
+            this.rabbitMQManager = RabbitMQManager.getInstance();
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException("Unable to initialize RabbitMQManager", e);
+        }
         startGarbageCollector();
     }
 
+    public static synchronized TopicOrchestrator getInstance() {
+        if (instance == null) {
+            instance = new TopicOrchestrator(5000); // Use the default timeout value you want for garbage collection
+        }
+        return instance;
+    }
+
     public static void publishMessage(TopicMessage message) throws IOException {
-        instance.topicMessages.computeIfAbsent(message.getType(), k -> new ConcurrentLinkedQueue<>()).add(message);
-        ViralService.getInstance().notifyNewTopicMessage(message); // Notify the Viral service
+        TopicOrchestrator orchestrator = getInstance();
+        orchestrator.topicMessages.computeIfAbsent(message.getType(), k -> new ConcurrentLinkedQueue<>()).add(message);
+        orchestrator.rabbitMQManager.publishTopicMessage(message); // Publish to RabbitMQ
     }
 
     public static List<TopicMessage> readMessages(String type) {
-        return new ArrayList<>(instance.topicMessages.getOrDefault(type, new ConcurrentLinkedQueue<>()));
+        TopicOrchestrator orchestrator = getInstance();
+        return new ArrayList<>(orchestrator.topicMessages.getOrDefault(type, new ConcurrentLinkedQueue<>()));
     }
 
     public static List<TopicMessage> getAllMessages() {
+        TopicOrchestrator orchestrator = getInstance();
         List<TopicMessage> allMessages = new ArrayList<>();
-        for (ConcurrentLinkedQueue<TopicMessage> queue : instance.topicMessages.values()) {
-            allMessages.addAll(queue);
-        }
+        orchestrator.topicMessages.values().forEach(allMessages::addAll);
         return allMessages;
     }
 
     public static int size(String type) {
-        ConcurrentLinkedQueue<TopicMessage> queue = instance.topicMessages.get(type);
+        TopicOrchestrator orchestrator = getInstance();
+        ConcurrentLinkedQueue<TopicMessage> queue = orchestrator.topicMessages.get(type);
         return (queue != null) ? queue.size() : 0;
     }
 
     private void startGarbageCollector() {
         new Thread(() -> {
-            while (true) {
+            while (!Thread.currentThread().isInterrupted()) {
                 try {
                     Thread.sleep(1000);
-
                     long currentTime = System.currentTimeMillis();
-                    instance.topicMessages.forEach((type, queue) -> {
-                        queue.removeIf(message -> currentTime - message.getSentTime() > instance.maxTimeout);
+                    topicMessages.forEach((type, queue) -> {
+                        queue.removeIf(message -> currentTime - message.getSentTime() > maxTimeout);
                     });
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
-                    return;
                 }
             }
         }).start();
