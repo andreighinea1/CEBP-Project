0a11a4f18fe86fc7d41b2f6fd16c480f6e67d13f
aghinea <aghinea@bitdefender.com>
2023-12-06 12:13:08 +0200

    Fixes for PR.

--- src/main/java/com/cebp_project/messenger/client/Client.java
+++ src/main/java/com/cebp_project/messenger/client/Client.java
@@ -27,11 +27,9 @@ public class Client implements Runnable {
     }
 
     public void receiveMessage(Message message) {
-        // Handle received message
         System.out.println(name + " received: " + message);
     }
 
-    // Method to handle received topic messages
     public void receiveTopicMessage(TopicMessage topicMessage) {
         System.out.println(name + " received topic message: " + topicMessage);
     }
@@ -39,49 +37,56 @@ public class Client implements Runnable {
     @Override
     public void run() {
         server.registerClient(name, this); // Register client with the server
-
         try {
-            // Sending mock messages with hashtags to other clients
-            String[] mockMessages = {
-                    "Hello from " + name + " #welcome",
-                    "Enjoying Java programming #java #coding"
-            };
-
-            for (String messageContent : mockMessages) {
-                for (String clientName : otherClients) {
-                    if (!clientName.equals(this.name)) {
-                        messageQueue.sendMessage(new Message(this.name, clientName, messageContent, System.currentTimeMillis()));
-                    }
-                }
-                // Simulate a random delay for sending messages
-                Thread.sleep(ThreadLocalRandom.current().nextInt(50, 250));
-            }
-
-            // Receiving messages
-            for (int i = 0; i < otherClients.size() - 1; i++) {
-                Message receivedMessage = messageQueue.receiveMessage(name);
-                if (receivedMessage != null) {
-                    System.out.println(name + " received from " + receivedMessage.getSender() + ": " + receivedMessage.getContent());
-                }
-            }
-
-            // Publish topic messages
-            TopicOrchestrator.getInstance().publishMessage(new TopicMessage("commonTopic", "FAST Broadcast from " + name));
-//            Thread.sleep(ThreadLocalRandom.current().nextInt(0, 3500));  // The msg won't expire
-            Thread.sleep(4500 + ThreadLocalRandom.current().nextInt(0, 1000));  // The msg will expire
-
-            // Publish a message to the topic
-            TopicOrchestrator.getInstance().publishMessage(new TopicMessage("commonTopic", "Broadcast from " + name));
-            // Simulate a random delay for listening to topic
-            Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 1500));
-
-            // Listening to the topic
-            List<TopicMessage> topicMessages = TopicOrchestrator.getInstance().readMessages("commonTopic");
-            System.out.println(name + " reads from topic: " + topicMessages);
+            sendMockMessages();
+            receiveMessages();
+            publishAndListenToTopicMessages();
         } catch (InterruptedException | IllegalStateException | IOException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error in Client [" + name + "]: " + e.getMessage());
             Thread.currentThread().interrupt();
         }
     }
 
+    private void sendMockMessages() throws InterruptedException, IOException {
+        // Sending mock messages with hashtags to other clients
+        String[] mockMessages = {
+                "Hello from " + name + " #welcome",
+                "Enjoying Java programming #java #coding"
+        };
+
+        for (String messageContent : mockMessages) {
+            for (String clientName : otherClients) {
+                if (!clientName.equals(this.name)) {
+                    messageQueue.sendMessage(new Message(this.name, clientName, messageContent, System.currentTimeMillis()));
+                }
+            }
+            // Simulate a random delay for sending messages
+            Thread.sleep(ThreadLocalRandom.current().nextInt(50, 250));
+        }
+    }
+
+    private void receiveMessages() {
+        for (int i = 0; i < otherClients.size() - 1; i++) {
+            Message receivedMessage = messageQueue.receiveMessage(name);
+            if (receivedMessage != null) {
+                System.out.println(name + " received from " + receivedMessage.getSender() + ": " + receivedMessage.getContent());
+            }
+        }
+    }
+
+    private void publishAndListenToTopicMessages() throws InterruptedException, IOException {
+        // Publish topic messages
+        TopicOrchestrator.getInstance().publishMessage(new TopicMessage("commonTopic", "FAST Broadcast from " + name));
+//        Thread.sleep(ThreadLocalRandom.current().nextInt(0, 3500));  // The msg won't expire
+        Thread.sleep(4500 + ThreadLocalRandom.current().nextInt(0, 1000));  // The msg will expire
+
+        // Publish a message to the topic
+        TopicOrchestrator.getInstance().publishMessage(new TopicMessage("commonTopic", "Broadcast from " + name));
+        // Simulate a random delay for listening to topic
+        Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 1500));
+
+        // Listening to the topic
+        List<TopicMessage> topicMessages = TopicOrchestrator.getInstance().readMessages("commonTopic");
+        System.out.println(name + " reads from topic: " + topicMessages);
+    }
 }
